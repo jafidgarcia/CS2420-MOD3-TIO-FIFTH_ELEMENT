d478bc402b953ba73667f84763f9f2e987b81f45
Jafid Garcia <jafidgarcia@gmail.com>
2023-05-31 16:53:41 -0600

    finished

--- FifthElement.cpp
+++ FifthElement.cpp
@@ -34,7 +34,27 @@ void FifthElement<Type>::insertNewFifthElement(const Type &value){
     if(this->count <4){
         this->insert(value);
     }
-}
+
+    // creating new node dynamically 
+    Node<Type>* insertFifth = new Node<Type>;
+    // stores the value passed in into the node 
+    insertFifth->data = value;
+
+    // create pointer that points to the first element
+    Node<Type>* currElement = this->front;
+
+    // loop currEl to the 5th position
+    for(int i = 1; i < 4; i++){
+        currElement = currElement->next;
+    }
+
+    // point to the right nodes 
+    insertFifth->next = currElement->next;
+    currElement->next = insertFifth;
+    // have to increment count from LinkedList?
+    // this->count++;
+    
+    }
 
 
 // can use the remove function inherited from LinkedList
@@ -45,6 +65,7 @@ void FifthElement<Type>::deleteFifthElement(){
     this->remove(4);
 }
 
+
 // nothing to inherit , will need to create
 template  <class Type>
 void FifthElement<Type>::swapFourthAndFifthElement(){
@@ -52,12 +73,16 @@ void FifthElement<Type>::swapFourthAndFifthElement(){
     Node<Type>* currElement = this->front;
 
     // loop currEl to the 4th position
-    for(int i =0; i < 3; i++){
+    for(int i = 1; i < 3; i++){
         currElement = currElement->next;
     }
     // creating pointer for 4th element and set it to currElement (4th right now because of loop)
     Node<Type>* fourthElement = currElement->next;
     Node<Type>* fifthElement = fourthElement->next;
 
+    // now we swap 4 & 5
+    currElement->next = fifthElement;
+    fourthElement->next = fifthElement->next;
+    fifthElement->next = fourthElement;
 
 }
